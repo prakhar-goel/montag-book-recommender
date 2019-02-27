e6d9f883230bd0886f2444874044e34404ee53f0
Prakhar Goel <git@prakhargoel.com>
2019-02-28 00:50:04 +0530

    added admin.py

--- webserver/montag/server/admin.py
+++ webserver/montag/server/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_year', 'publisher')
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'rating')
+    pass
+
+@admin.register(Recommender)
+class RecommenderAdmin(admin.ModelAdmin):
+    list_display = ('version', 'model')
